d2888a46116d2bc1bd515de4a6c2981a8a6dab32
kimanh2610 <huynhanh2215@gmail.com>
2022-02-15 17:23:12 +0700

    init Project

---
+++ src/config/viewEngine.js
@@ -0,0 +1,12 @@
+//config thư viên viewEngine
+import express from "express";
+
+let configViewEngine = (app) => {
+    //arrow function
+    app.use(express.static("./src/public"));
+    app.set(" view engine", "ejs");
+    app.set("views", "./src/views");
+    
+}
+
+module.exports = configViewEngine;
