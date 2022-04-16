b3d1333cbf9adaf71912c79c60005329deaf8498
HuynhKimAnh <huynhkimanhh2610@gmail.com>
2022-04-17 01:05:05 +0700

    phan quyen admin #55

--- src/services/userService.js
+++ src/services/userService.js
@@ -23,7 +23,7 @@ module.exports.handleUserLogin = (email, password) => {
             if (isExist) {
                 //user already exists
                 let user = await db.User.findOne({
-                    attributes: ['email', 'roleId', 'password'],
+                    attributes: ['email', 'roleId', 'password', 'fullName'],
                     where: { email: email },
                     raw: true
                 });
