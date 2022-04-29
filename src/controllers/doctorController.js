4f5d948868278e3f7f0f6d326521789b07590263
HuynhKimAnh <huynhkimanhh2610@gmail.com>
2022-04-29 21:33:31 +0700

    complete detail doctor #67

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -2,11 +2,11 @@ import doctorService from "../services/doctorService";
 
 let getTopDoctorHome = async (req, res) => {
     let limit = req.query.limit;
-    if(!limit) limit = 10;
-    try{
+    if (!limit) limit = 10;
+    try {
         let response = await doctorService.getTopDoctorHome(+limit);
         return res.status(200).json(response);
-    }catch(e){
+    } catch (e) {
         console.log(e);
         return res.status(200).json({
             errCode: -1,
@@ -15,6 +15,35 @@ let getTopDoctorHome = async (req, res) => {
     }
 }
 
+let getAllDoctors = async (req, res) => {
+    try {
+        let doctors = await doctorService.getAllDoctors();
+      
+        return res.status(200).json(doctors);
+    }catch (e) {
+       
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error form the server'
+        })
+    }
+}
+
+let postInforDoctors = async (req, res) => {
+    try{
+        let response = await doctorService.saveDetailInforDoctor(req.body);
+        return res.status(200).json(response);
+    }catch(e) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error form the server'
+        })
+    }
+
+}
+
 module.exports = {
-    getTopDoctorHome: getTopDoctorHome,
+    getTopDoctorHome,
+    getAllDoctors,
+    postInforDoctors,
 }
